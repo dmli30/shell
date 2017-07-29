b7b7741f144f0f57153d7b612be0a6b363c1e7d4
dmli30 <dmlifei@gmail.com>
2017-07-29 18:08:25 +0800

    Create ngx_status.sh
    zabbix监控nginx状态脚本

---
+++ ngx_status.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+URL="http://192.168.7.227:88/ngx_status"
+FILE="/tmp/ngx_status.txt"
+
+function active(){
+/usr/bin/curl -s $URL |grep Active|awk '{print $3}'
+}
+
+function reading(){
+/usr/bin/curl -s $URL |grep Reading|awk '{print $2}'
+}
+
+function writing(){
+/usr/bin/curl -s $URL |grep Writing|awk '{print $4}'
+}
+
+function waiting(){
+/usr/bin/curl -s $URL |grep Waiting|awk '{print $6}'
+}
+
+function requests(){
+/usr/bin/curl -s $URL |awk '{if(NR==3) print $3}'
+}
+
+function dropped(){
+/usr/bin/curl -s $URL > $FILE
+accepts=`awk '{if(NR==3) print $1}' $FILE`
+handled=`awk '{if(NR==3) print $2}' $FILE`
+drop=`expr $accepts - $handled`
+echo "$drop"
+}
+
+function ping(){
+/usr/sbin/pidof nginx|wc -l
+}
+
+#根据脚本参数执行对应应函数
+$1
